6c646a55da6bb7eed5edafda9a50cb3f4146dd64
Ryan Dines <rdines@accelevant.com>
2020-07-07 12:06:08 -0400

    End of day.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, Keyboard, Platform  } from 'react-native';
 
 export default function App() {
 
   const [value, onChangeText] = React.useState('Useless Placeholder');
   
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView  
+    style={styles.container} 
+    onTouchStart={()=>{
+      Keyboard.dismiss();
+    }}
+    behavior={Platform.OS == "ios" ? "padding" : "height"}
+    >
+      
       <Text>Application Bare Bones</Text>
       <TextInput
         style={{ height: 40, borderColor: 'grey', borderWidth: 1, width: 200, borderRadius:3, paddingLeft: 17 }}
@@ -15,7 +22,7 @@ export default function App() {
         value={value}
       />      
       <StatusBar style="auto" />
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
