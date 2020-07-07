caf257ed45405eb77607c90aaff944ddf865ccf8
Ryan Dines <ryan.dines@gmail.com>
2020-07-07 16:52:32 -0400

    Working copy of linear gradient that respects keyboard movement.

--- App.tsx
+++ App.tsx
@@ -1,34 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, Keyboard, Platform  } from 'react-native';
+import { StyleSheet, Text, Image, TextInput, KeyboardAvoidingView, Keyboard, Platform, Dimensions } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+export const deviceHeight = Dimensions.get("window").height;
+export const deviceWidth = Dimensions.get("window").width;
 
 export default function App() {
 
   const [value, onChangeText] = React.useState('Useless Placeholder');
-  
+
   return (
-    <KeyboardAvoidingView  
-    style={styles.container} 
-    onTouchStart={()=>{
-      Keyboard.dismiss();
-    }}
-    behavior={Platform.OS == "ios" ? "padding" : "height"}
+    <KeyboardAvoidingView
+      style={styles.container}
+      onTouchStart={() => {
+        Keyboard.dismiss();
+      }}
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
     >
-      
-      <Text>Application Bare Bones</Text>
-      <TextInput
-        style={{ height: 40, borderColor: 'grey', borderWidth: 1, width: 200, borderRadius:3, paddingLeft: 17 }}
-        onChangeText={text => onChangeText(text)}
-        value={value}
-      />      
-      <StatusBar style="auto" />
+      <LinearGradient
+        colors={['#5A5AA2', '#36366F']}
+        style={{ 
+          flex:1 , 
+          alignItems: 'center', 
+          justifyContent: "center", 
+          width: deviceWidth
+        }}
+        >
+          <Image 
+            source={require('./src/components/images/watchpointBanner.png')}
+            style={{...StyleSheet.absoluteFillObject}}
+        />
+        <Text>Application Bare Bones</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'grey', borderWidth: 1, width: 200, borderRadius: 3, paddingLeft: 17 }}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+        />
+        <StatusBar style="auto" />
+        </LinearGradient>
     </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+        container: {
+        flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
