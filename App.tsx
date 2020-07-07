dfd4da78876a8c45042a63b13e6c50c6ac72dde7
Ryan Dines <rdines@accelevant.com>
2020-07-07 11:28:31 -0400

    Initial commit.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [value, onChangeText] = React.useState('Useless Placeholder');
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Application Bare Bones</Text>
+      <TextInput
+        style={{ height: 40, borderColor: 'grey', borderWidth: 1, width: 200, borderRadius:3, paddingLeft: 17 }}
+        onChangeText={text => onChangeText(text)}
+        value={value}
+      />      
       <StatusBar style="auto" />
     </View>
   );
